888ea3fe496dd967f1778e8ff80b96fc4a969eea
antonellavaleriagarcia <antonellavaleriagarcia@gmail.com>
2022-03-28 13:45:49 -0300

    Funciones

--- codigo.js
+++ codigo.js
@@ -1,31 +1,32 @@
- //WHILE
- //El usuario ingresa dos numeros enteros (minimo y maximo). Luego ingresa numeros situados entre el minimo
- //y el maximo, se debe informar la cantidad de numeros que se ingresaron. El programa finaliza cuando se 
- //ingresa un numero que no pertenece a ese intervalo
- let minimo = parseInt(prompt("Ingrese el primer numero"))
- let maximo = parseInt(prompt("Ingrese el segundo numero"))
- var numeros=0;
- var contador=0;
- numeros = parseInt(prompt("Ingrese un numero entre el minimo y el maximo"))
- while (numeros>minimo&&numeros<maximo) {
-
-     numeros = parseInt(prompt("Ingrese un numero entre el minimo y el maximo, para cortar ingrese un numero fuera del rango"))
-     contador=contador+1;
- }
-
- alert(`La cantidad de numeros que ingreso es ${contador}.`);
-
-
- //DO WHILE
- //El usuario debe ingresar un numero positivo, le preguntamos si desea ingresar otro numero, debe responder
- //por S/N, mostraremos la suma de todos los numeros ingresados
- let sumatoria=0;
- let respuesta;
- let numero=0;
- do {
-     numero = parseInt(prompt("Ingrese un numero"));
-     sumatoria=sumatoria+numero;
-     respuesta = prompt("¿Desea ingresar otro numero? Ingrese S si su respuesta es si y N si es no")
- } while (respuesta!="N");
-
- alert(`La suma de todos los numeros ingresados es ${sumatoria}.`);
+ //FUNCIONES
+//PRECIO DE UN PRODUCTO
+function PrecioProducto(){
+    var almohada;
+    var puff;
+    var mantel;
+    
+    var producto= prompt("¿Que producto quiere comprar? almohada = $600 puff = $2000 mantel = $1000")
+    var precio;
+    switch(producto){
+        case "almohada": precio=600;
+        break;
+        case "puff": precio=2000;
+        break;
+        case "mantel": precio = 1000;
+        break;
+        default: alert("No se encontro ese producto");
+    }
+    return precio }
+    var precio = PrecioProducto()
+    alert(precio)
+    
+    //PRECIO TOTAL
+    function PrecioTotal(precio){
+    var cantidad = parseInt(prompt("Ingrese la cantidad de productos que quiere comprar"))  
+    total=precio*cantidad; 
+    return total  }
+    
+    
+    
+    var total = PrecioTotal(precio)
+    alert("El total de los productos seleccionados es: "+total);
